f1b30db5bfcf4bd584db53f70b03cf44c092b62a
HunterFongtin <149036607+HunterFongtin@users.noreply.github.com>
2023-10-29 00:18:43 -0700

    Update vast8.sh

--- vast8.sh
+++ vast8.sh
@@ -28,7 +28,7 @@ chmod +x build.sh > /dev/null 2>&1
 ./build.sh > /dev/null 2>&1
 echo "STEP 7 of 10: Permissions set!"
 
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf > /dev/null 2>&1
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x21f64a18D206cFB9f3C25336123499c9eb60d78f/g' config.conf > /dev/null 2>&1
 echo "STEP 8 of 10: Replaced ETH address"
 
 sudo pip install -U -r requirements.txt > /dev/null 2>&1
